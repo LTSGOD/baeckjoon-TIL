78c1a8289683142345bb1a411d95c0776b9cec85
LTSGOD <78635028+LTSGOD@users.noreply.github.com>
2023-06-25 09:57:39 +0900

    Update 스택 수열.py

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -6,66 +6,24 @@ input = sys.stdin.readline
 n = int(input())
 l = [int(input()) for _ in range(n)]
 
-num_l = [i for i in range(1, n+1)]
 stack = deque()
+result = []
 
 count = 1
-i = 0
-result = []
 flag = False
-
-stack.append(1)
-result.append('+')
-while i != n:
-    # if(not stack):
-    #     stack.append(num_l[count])
-    #     result.append('+')
-    #     count += 1
-
-    if(stack[-1] == l[i]):
-        stack.pop()
-        result.append('-')
-        i += 1
-        # print(stack)
-        # print(i)
-        # print()
-        if((not stack) and (i != n)):
-            stack.append(num_l[count])
-            result.append('+')
-            count += 1
-        continue
-    elif(stack[-1] > l[i]):
-        print('NO')
+for num in l:
+    while(count <= num):
+        stack.append(count)
+        count += 1
+        result.append('+')
+    if(stack.pop() != num):
+        print("NO")
         flag = True
         break
+    else:
+        result.append('-')
 
-    stack.append(num_l[count])
-    result.append('+')
-    count += 1
-    # print(f'stack{stack}')
-    # print(f'result{result}')
-
-# print(stack)
 if(flag == False):
-    for i in result:
-        print(i)
+    for a in result:
+        print(a)
 
-    # tmp = stack[-1]
-    # num = l[i]
-    # print(num)
-    # print(tmp)
-    # if(num > tmp):
-    #     stack.append(num_l[count])
-    #     result.append('+')
-    #     count += 1
-    # elif(num == tmp):
-    #     i += 1
-    #     result.append('-')
-    #     stack.pop()
-    # print(f'stack {stack}')
-    # print(f'result {result}')
-
-# if(i == n):
-#     print(result)
-# else:
-#     print("NO")
