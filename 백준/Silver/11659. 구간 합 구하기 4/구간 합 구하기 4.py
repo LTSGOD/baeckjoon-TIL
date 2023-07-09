b8abf5f7bd30e57affcafcc28211628076af0529
LTSGOD <78635028+LTSGOD@users.noreply.github.com>
2023-07-09 11:31:16 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 356 ms, Memory: 135956 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -0,0 +1,19 @@
+import sys
+from heapq import heappush, heappop
+
+input = sys.stdin.readline
+
+N, M = list(map(int, input().split()))
+
+tmp = 0
+
+num_l = [0]
+for num in list(map(int, input().split())):
+    num_l.append(tmp + num)
+    tmp += num
+
+querys = [list(map(int, input().split())) for _ in range(M)]
+
+for query in querys:
+    start, end = query
+    print(num_l[end]-num_l[start-1])
