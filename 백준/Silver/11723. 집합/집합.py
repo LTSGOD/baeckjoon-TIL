307a561edbd4a5d5f77c3f6a71c96944be00efcd
LTSGOD <78635028+LTSGOD@users.noreply.github.com>
2023-08-18 22:21:44 +0900

    [Silver V] Title: 집합, Time: 5212 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -6,26 +6,25 @@ M = int(input())
 
 result = set()
 
-tmp = set([str(i) for i in range(1, 21)])
 for _ in range(M):
-    command = list(input().split())
-
-    if(command[0] == "add"):
-        result.add(command[1])
-    elif(command[0] == "remove"):
-        if(command[1] in result):
-            result.remove(command[1])
-    elif(command[0] == "check"):
-        if(command[1] in result):
-            print(1)
+    command = input().rstrip().split()
+    
+    if command[0] == "add":
+        result.add(int(command[1]))
+    elif command[0] =="check":
+        if int(command[1]) in result:
+            print("1")
         else:
-            print(0)
-    elif(command[0] == "toggle"):
-        if(command[1] in result):
-            result.remove(command[1])
+            print("0")
+    elif command[0] == "remove":
+        if int(command[1]) in result:
+            result.discard(int(command[1]))
+    elif command[0] == "toggle":
+        if int(command[1]) in result:
+            result.discard(int(command[1]))
         else:
-            result.add(command[1])
-    elif(command[0] == "all"):
-        result = tmp
-    else:
+            result.add(int(command[1]))
+    elif command[0] == "all":
+        result = set([i for i in range(1,21)])
+    elif command[0] == "empty":
         result = set()
