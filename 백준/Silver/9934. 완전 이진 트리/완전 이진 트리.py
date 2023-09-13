b661f5b8d559422d495568f27aa76440e21d8680
LTSGOD <78635028+LTSGOD@users.noreply.github.com>
2023-09-13 16:11:25 +0900

    [Silver I] Title: 완전 이진 트리, Time: 156 ms, Memory: 116244 KB -BaekjoonHub

--- 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.py
+++ 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.py
@@ -1,28 +1,51 @@
 import sys
 from collections import deque
-
 input = sys.stdin.readline
 
 N = int(input())
 
-array = deque(map(int, input().split()))
 
-array.appendleft(0)
+#################### 재귀 풀이 #######################
+array = list(map(int, input().split()))
 
-mid = len(array)//2
+level = [[] for _ in range(N)]
 
-result = deque()
-i = 1
-j = 2
-while i <= mid:
-    tmp = []
-    for index in range(i, len(array), j):
-        tmp.append(array[index])
-    j *= 2
-    i *= 2
-    result.appendleft(tmp)
+def recursion(arr, depth):
+    if depth > N - 1:
+        return
 
-for l in result:
+    mid = len(arr) // 2
+    level[depth].append(arr[mid])
+    recursion(arr[:mid], depth + 1)
+    recursion(arr[mid:], depth + 1)
+
+recursion(array, 0)
+
+for l in level:
     for s in l:
         print(s,end=" ")
     print()
+
+
+################## 재귀 X 풀이 ####################
+# array = deque(map(int, input().split()))
+
+# array.appendleft(0)
+
+# mid = len(array)//2
+
+# result = deque()
+# i = 1
+# j = 2
+# while i <= mid:
+#     tmp = []
+#     for index in range(i, len(array), j):
+#         tmp.append(array[index])
+#     j *= 2
+#     i *= 2
+#     result.appendleft(tmp)
+
+# for l in result:
+#     for s in l:
+#         print(s,end=" ")
+#     print()
