8028ccbf899a0a15e45575d1018e15ed3e992ecf
LTSGOD <78635028+LTSGOD@users.noreply.github.com>
2023-10-15 00:28:57 +0900

    [Gold II] Title: 트리의 지름, Time: 912 ms, Memory: 97836 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.py
@@ -6,6 +6,8 @@ input = sys.stdin.readline
 
 V = int(input())
 
+#트리에서의 임의의 점에서 가장 먼 노드는 가장 먼거리 노드 양 끝 점 중 하나이다.
+
 edge = [[] for _ in range(V + 1)]
 start_node = []
 
@@ -19,7 +21,7 @@ for _ in range(V):
 
   
 
-def dfs(start):
+def bfs(start):
     queue = deque()
 
     queue.append((start,0))
@@ -37,12 +39,14 @@ def dfs(start):
 
 answer = []
 
+#임의의 점 1에서 가장먼 점 선정
 visited = [False for _ in range(V+1)]
-dfs(1)
+bfs(1)
 
 answer.sort()
 visited = [False for _ in range(V+1)]
 
-dfs(answer[-1][1])
+#가장 먼점에서 가장 먼거리를 구하면 그것이 최대의 트리의 지름
+bfs(answer[-1][1])
 answer.sort()
 print(answer[-1][0])
