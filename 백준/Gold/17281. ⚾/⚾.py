9a564ed8a951de8530289e0a8efb97c4a6ae3297
LTSGOD <78635028+LTSGOD@users.noreply.github.com>
2023-11-05 13:06:12 +0900

    [Gold IV] Title: ⚾, Time: 1028 ms, Memory: 121676 KB -BaekjoonHub

--- 백준/Gold/17281. ⚾/⚾.py
+++ 백준/Gold/17281. ⚾/⚾.py
@@ -1,59 +1,100 @@
-from itertools import permutations  
-import sys   
-input = sys.stdin.readline  
-  
-  
-N = int(input())  
-# 최댓값 저장  
-cnt = 0  
-  
-hit_info = [[*map(int, input().split())] for _ in range(N)]  
-  
-# 2~9번 타자 순열 만들어주기  
-for taja in permutations((range(1 , 9)), 8):  
-    # 4번 타자는 0번인덱스 선수 고정이므로 추가해주기  
-    taja = list(taja[:3]) + [0] + list(taja[3:])  
-  
-    # 현재 타자  
-    hitter = 0  
-    # 이번 이닝 점수  
-    result = 0  
-    for i in range(N):  
-        out = 0  
-        base = [0,0,0,0]  
-  
-        # 3아웃 까지 돌려준다.  
-        while out < 3:  
-            # 현재 이닝 타자 정보에서 현재 타자 불러와주기  
-            hit = hit_info[i][taja[hitter]]  
-  
-            # 아웃, 1루, 2루, 3루, 홈런 일 때의 조건문 작성  
-            if hit == 0:  
-                out += 1  
-  
-            # 안타일 때 3루에 주자가 있다면 +1  
-            # 이후 베이스 정보 갱신            
-            elif hit == 1:  
-                result += base[3]  
-                base = [0, 1, base[1], base[2]]  
-  
-            # 2루타인 경우 2, 3루 주자만큼 +1  
-            elif hit == 2:  
-                result += base[2] + base[3]  
-                base = [0, 0, 1, base[1]]  
-  
-            elif hit == 3:  
-                result += base[1] + base[2] + base[3]  
-                base = [0, 0, 0, 1]  
-  
-            elif hit == 4:  
-                result += base[1] + base[2] + base[3] + 1  
-                base = [0, 0, 0, 0]  
-  
-            # 다음 타자 불러오기 위해 +1 해준 후 9로 나눠주면 된다.  
-            hitter = (hitter+1) % 9  
-  
-    if result > cnt:  
-        cnt = result  
-  
-print(cnt)
+import sys
+from collections import deque
+from itertools import permutations
+
+input = sys.stdin.readline
+
+
+#일단 노가다로 모든 경우를 가정해서 풀면 최대 7P7(타선뽑기) * 50(이닝수계산) * 최대타수(27 1명만 아웃될때)
+# 54432000 으로 브루트 포스 가능할듯?
+# 1. base 점수 세는 거 고침
+# 2. 다음 타자 번호 if문 제거
+# 3. score 점수 매기는거 boolean 에서 정수로 변경
+# 4. insert 연산이 오래걸리는 것같아서 변경
+# 5. map 연산 변경 -> 상관ㅇ없음
+# 6. 계속 꺼내지 말고 한번 만 꺼내서 확인
+# 7. sum(함수 생략)
+
+# 순열 구현
+def permutation(depth):
+
+    if depth == 8:
+        hitter_order.append([*result])
+        return
+
+    for i in range(2,10):
+
+        if visited[i]:
+            continue
+
+        result.append(i)
+        visited[i] = True
+        permutation(depth+1)
+        visited[i] = False
+        result.pop()
+visited = [False for _ in range(10)]
+
+#타순 모든 경우의 수 구하기
+hitter_order = []
+
+result = []
+
+permutation(0)
+
+N = int(input())
+info = []
+for _ in range(N):
+    info.append(list(map(int, input().split())))
+
+max_score = -1
+
+
+#가능한 모든 타순 계산
+for player_list in hitter_order:
+
+    score = 0
+
+    #1번타자 추가
+    player_list = list(player_list[:3]) + [1] + list(player_list[3:])
+
+    현재타자 = 0
+
+    #게임 시작
+    for 한이닝_득점정보 in info:
+
+        #아웃카운트와 베이스는 이닝마다 초기화
+        out = 0
+        base = [0,0,0]
+
+        
+        while out < 3:
+
+            hit = 한이닝_득점정보[player_list[현재타자]-1]
+            
+            #타순에 따라 한이닝득점정보에 따라 계산
+            if hit == 0:
+                out += 1
+            elif hit == 1:
+                score += base[2]
+                base = [1, base[0], base[1]]
+            elif hit == 2:
+                score += base[1] + base[2]
+                base = [0, 1, base[0]]
+            elif hit == 3:
+                score += base[0] + base[1] + base[2]
+                base = [0, 0, 1]
+            elif hit == 4:
+                score += base[0] + base[1] + base[2]  + 1
+                base = [0,0,0]
+            
+            #최적화
+            현재타자 += 1
+
+            if 현재타자 > 8:
+                현재타자 = 0
+
+    #최댓값 갱신 
+    if score > max_score:
+        max_score = score
+
+print(max_score)
