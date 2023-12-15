ea85965aa1becb95c9fdd2b5681f369eca00edf8
LTSGOD <78635028+LTSGOD@users.noreply.github.com>
2023-12-15 17:26:49 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.py
@@ -0,0 +1,65 @@
+#dlru
+#모든경우 판단해보고 출력
+import sys
+sys.setrecursionlimit(10 ** 8)
+
+def recursion(depth,n,m,current_x,current_y,r,c, k):
+    if depth == k:
+        if current_x == r and current_y == c:
+            return True
+        return False
+    
+    #현재 있는 거리에서 더이상 도착지에 갈 수 없다면
+    if abs(r-current_x)+ abs(c - current_y) > (k - depth):
+        return False
+    
+    for d in ['d','l','r','u']:
+        
+        #경계 벗어나는경우
+        if d == 'd':
+            if current_x + 1 > n:
+                continue
+            else:
+                current_x += 1
+        elif d =='l':
+            if current_y - 1 < 1:
+                continue
+            else:
+                current_y -= 1
+        elif d== 'r':
+            if current_y + 1 > m:
+                continue
+            else:
+                current_y += 1
+        else:
+            if current_x - 1 < 1:
+                continue
+            else:
+                current_x -= 1
+        
+        result.append(d)
+        if recursion(depth+1, n,m, current_x, current_y,r,c,k):
+            return True
+        result.pop()
+        
+        if d == 'd':
+            current_x -= 1
+        elif d == 'l':
+            current_y += 1
+        elif d == 'r':
+            current_y -= 1
+        else:
+            current_x += 1
+        
+def solution(n, m, x, y, r, c, k):
+    answer = ''
+    global result
+    result = []
+
+    if k < abs(r-x) + abs(c-y):
+        return "impossible"
+    if abs(k - abs(r-x) + abs(c-y)) % 2 != 0:
+        return "impossible"
+    recursion(0,n,m,x,y,r,c,k)
+    
+    return "".join(result)
