5d4ba62d16a332d4efee68944090af0ee9916c13
LTSGOD <78635028+LTSGOD@users.noreply.github.com>
2023-12-23 14:11:21 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -0,0 +1,40 @@
+
+def check(a, b):
+    
+    count = 0
+    for i in range(len(a)):
+        if a[i] == b[i]:
+            count += 1
+    
+    #한글자만 다른경우
+    if count == (len(a) - 1):
+        return True
+    else:
+        return False
+        
+def recursion(begin, words, target):
+    global answer
+    if begin == target:
+        return True
+    
+    for i,w in enumerate(words):
+        if visited[i]:
+            continue
+        if check(begin, w):
+            visited[i] = True
+            answer += 1
+            if recursion(w, words, target):
+                return True
+            answer -= 1
+
+    return False
+def solution(begin, target, words):
+    global visited
+    global answer
+    visited = [False for _ in words]
+    answer = 0
+    if recursion(begin, words, target):
+        return answer
+    else:
+        return 0
+  
