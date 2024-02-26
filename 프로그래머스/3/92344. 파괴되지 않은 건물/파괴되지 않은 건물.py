abb6c84fcd6e43e149faa949b08e6ccacb93bf22
LTSGOD <78635028+LTSGOD@users.noreply.github.com>
2024-02-26 20:18:38 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 592.40 ms, Memory: 102 MB -BaekjoonHub

---
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
@@ -0,0 +1,28 @@
+def solution(board, skill):
+    answer = 0
+    
+    tmp = [[0 for _ in range(len(board[0]) + 1)] for _ in range(len(board) + 1)]
+    
+    for t, r1, c1, r2, c2, d in skill:
+        if t == 1:
+            d = -d 
+        tmp[r1][c1] += d
+        tmp[r1][c2 + 1] -= d
+        tmp[r2 + 1][c1] -= d
+        tmp[r2 +1][c2 + 1] += d
+    
+    
+    for i in range(len(tmp)):
+        for j in range(1, len(tmp[0])):
+            tmp[i][j] += tmp[i][j - 1]
+            
+    for j in range(len(tmp[0])):
+        for i in range(1, len(tmp)):
+            tmp[i][j] += tmp[i - 1][j]
+
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] + tmp[i][j] > 0:
+                answer += 1
+            
+    return answer
