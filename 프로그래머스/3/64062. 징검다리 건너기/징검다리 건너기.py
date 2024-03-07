68cc4a6818398e7806f10942ac25edaef8cb79a3
LTSGOD <78635028+LTSGOD@users.noreply.github.com>
2024-03-07 19:46:14 +0900

    [level 3] Title: 징검다리 건너기, Time: 310.52 ms, Memory: 18.6 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -3,6 +3,7 @@ def check(stones,mid, k):
     count = 0 
     
     for i in range(0, len(stones)):
+        
         if stones[i] - mid < 1:
             count += 1
         else:
@@ -11,12 +12,12 @@ def check(stones,mid, k):
         if count == k:
             return False
     return True
+
 def solution(stones, k):
     answer = 0
     
     start = 0
     end = max(stones)
-    
     while start + 1 < end:
         
         mid = (start + end) // 2
